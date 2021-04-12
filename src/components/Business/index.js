2ab0397d6028e45fdd7e192c1721f1db2402845f
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-04-12 09:16:26 +0200

    corrected typo on business page

--- src/components/Business/index.js
+++ src/components/Business/index.js
@@ -3,10 +3,9 @@ import Button from "react-bootstrap/Button";
 import Jumbotron from "react-bootstrap/Jumbotron";
 
 export default function Business(props) {
-  const business = props.business;
   return (
     <Jumbotron>
-      {business.businessName}
+      {props.name}
       <Button> Make Appointment </Button> <Button> Contact Business </Button>
     </Jumbotron>
   );
