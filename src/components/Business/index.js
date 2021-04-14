d3a2b1458f7267dc47dcd1b9f8a32c27b3e12def
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-04-14 10:27:57 +0200

    edited the business component

--- src/components/Business/index.js
+++ src/components/Business/index.js
@@ -1,12 +1,38 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { selectUser } from "../../store/user/selectors";
+import { makeAppointment } from "../../store/appointments/actions";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 export default function Business(props) {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  const userLoggedIn = () => {
+    return user.name !== null;
+  };
+
+  const doMakeAppointment = (event) => {
+    event.preventDefault();
+    dispatch(makeAppointment(props.date, props.id));
+  };
+
   return (
     <Jumbotron>
       {props.name}
-      <Button> Make Appointment </Button> <Button> Contact Business </Button>
+      <div>
+        {userLoggedIn() ? (
+          <Button onClick={doMakeAppointment}>Make an appointment</Button>
+        ) : (
+          <Link to="/login" style={{ textAlign: "center" }}>
+            <Button>login to make an appointment</Button>
+          </Link>
+        )}
+      </div>{" "}
+      <Button> Contact Business </Button>
     </Jumbotron>
   );
 }
